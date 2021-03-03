2a90d144c50d922c16e26737a24b646e7e1352ac
anishthk82 <Anish@DESKTOP-QIQ3KHU>
2021-03-02 23:11:29 -0500

    testing another project with git

---
+++ testgithub-1/src/main/java/com/example/testgithub/Testgithub1Application.java
@@ -0,0 +1,14 @@
+package com.example.testgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testgithub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testgithub1Application.class, args);
+		System.out.println("Pushing another project to git");
+	}
+
+}
